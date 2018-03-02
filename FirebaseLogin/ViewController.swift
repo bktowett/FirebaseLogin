073ed4399074e4b5f0d82191eb3ae90baa71093b
bktowett <bktowett@gmail.com>
2018-03-02 16:40:16 +0300

    works

--- FirebaseLogin/ViewController.swift
+++ FirebaseLogin/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        if Auth.auth().currentUser != nil{
+            self.presentLoggeInScreen()
+        }
     }
-
-
+    @IBAction func createAccountTapped(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+          Auth.auth().createUser(withEmail: email, password: password, completion: {(user,error) in
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            if let firebaseError = error{
+                print(firebaseError.localizedDescription)
+                return
+            }
+            self.presentLoggeInScreen()
+            
+          })
+        }
+    }
+    @IBAction func loginTapped(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            Auth.auth().signIn(withEmail: email, password: password, completion: {(user, error) in
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                if let firebaseError = error{
+                    print(firebaseError.localizedDescription)
+                    return
+                }
+                self.presentLoggeInScreen()
+            })
+        }
+    }
+    
+    func presentLoggeInScreen(){
+        let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loggedInViewController:loggedInViewController = storyboard.instantiateViewController(withIdentifier: "loggedInViewController") as! loggedInViewController
+        self.present(loggedInViewController, animated: true, completion: nil)
+    }
 }
 
---
+++ FirebaseLogin/loggedInViewController.swift
@@ -0,0 +1,45 @@
+//
+//  loggedInViewController.swift
+//  FirebaseLogin
+//
+//  Created by Brian Kiptoo on 02/03/2018.
+//  Copyright © 2018 Brian Kiptoo. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class loggedInViewController: UIViewController {
+
+    @IBAction func logOutTapped(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            dismiss(animated: true, completion: nil)
+        }catch{
+           print("there was a problem logging out")
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
